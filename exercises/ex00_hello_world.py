ecd784fce57a76fdb56be60c352417ad9fed18c2
arsh <arshnoman2270@gnail.com>
2024-08-22 18:46:00 -0400

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,9 @@
+"""My first exercise in COMP110!"""
+
+__author__ = "730815627"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+
+    return "Hello, " + name + "!"
